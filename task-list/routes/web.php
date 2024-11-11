9d47ce642e2e53b01d8a16b8d9dc352312a1be3b
Jimmy Yen <ichthyoid95@gmail.com>
2024-11-11 22:09:16 +0800

    [Project #1 task_list] all about routing

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+})->name('hello');
+
+Route::get('/hallo', function () {
+    return redirect('/hello');
+});
