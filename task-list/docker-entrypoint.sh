d7c2232ace73166a16924df14dcf07c1ab90ddcf
Jimmy <jimmy@vipcartw.com>
2024-11-12 11:36:16 +0800

    [Project #1 task_list] template & adding mysql to docker-compose

--- task-list/docker-entrypoint.sh
+++ task-list/docker-entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# 等待 SQLite 容器準備就緒
-sleep 5
-
-# 建立 SQLite 資料庫檔案（如果不存在）
-if [ ! -f /var/www/html/database/database.sqlite ]; then
-    mkdir -p /var/www/html/database
-    touch /var/www/html/database/database.sqlite
-    chmod 777 /var/www/html/database/database.sqlite
-fi
-
 # 執行遷移和填充資料
 php artisan migrate --force
 
---
+++ task-list/resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'The list of tasks')
+
+@section('content')
+
+<div>
+    {{-- @if (count($tasks)) --}}
+    @forelse ($tasks as $task)
+        <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a><br>
+    @empty
+        <p>There is no tasks!</p>
+    @endforelse
+    {{-- @endif --}}
+</div>
+
+@endsection
