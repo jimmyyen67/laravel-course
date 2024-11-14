bbee79b23ba56d2e5dbffe8289f3dc576b99a1a1
Jimmy <jimmy@vipcartw.com>
2024-11-14 16:29:19 +0800

    [Project #2 book_reviews] models and queries

--- book-reviews/app/Models/Book.php
+++ book-reviews/app/Models/Book.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Book extends Model
 {
@@ -12,5 +13,57 @@ class Book extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    public function scopePopular(Builder $query, string $from = null, string $to = null): Builder
+    {
+        return $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query, string $from = null, string $to = null): Builder
+    {
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder
+    {
+        return $query->having('reviews_count', '>-', $minReviews);
+    }
+
+    /**
+     * Filter reviews by date range
+     * @param Builder $query
+     * @param string|null $from
+     * @param string|null $to
+     * @return Builder
+     */
+    private function dateRangeFilter(Builder $query, string $from = null, string $to = null): Builder
+    {
+        if ($from) {
+            $query->where('created_at', '>=', $from);
+        }
+        if ($to) {
+            $query->where('created_at', '<=', $to);
+        }
+        return $query;
+    }
+
+    public function scopeCreatedAfter(Builder $query, string $date): Builder
+    {
+        return $query->where('created_at', '>=', $date);
+    }
+
+    public function scopeCreatedBefore(Builder $query, string $date): Builder
+    {
+        return $query->where('created_at', '<', $date);
     }
 }
