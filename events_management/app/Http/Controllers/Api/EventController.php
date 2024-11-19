f1e48d60095bbe5443da9af2d545f687f11f98d1
JimmyYen <ichthyoid95@gmail.com>
2024-11-19 14:17:31 +0800

    [Project #3 event_management_api] update and delete event

--- events_management/app/Http/Controllers/Api/EventController.php
+++ events_management/app/Http/Controllers/Api/EventController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Event;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -11,7 +13,7 @@ class EventController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(): \Illuminate\Database\Eloquent\Collection
+    public function index(): Collection
     {
         return Event::all();
     }
@@ -30,7 +32,6 @@ class EventController extends Controller
             ]),
             'user_id' => 1, // 暫時把新增的活動都先掛在 user_id 1 身上
         ]);
-
         return $event;
     }
 
@@ -45,16 +46,25 @@ class EventController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event): Event
     {
-        //
+        $event->update(
+            $request->validate([
+                'name' => 'sometimes|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'sometimes|date',
+                'end_time' => 'sometimes|date|after:start_time',
+            ])
+        );
+        return $event;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event): JsonResponse
     {
-        //
+        $event->delete();
+        return response()->json(status: 204); // 無論是否有找到該ID的活動，接返回204成功、並且不回傳任何內容
     }
 }
