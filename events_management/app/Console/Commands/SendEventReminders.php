21b0d12037c830afd2c5a20a208ef2fef6330fb0
JimmyYen <ichthyoid95@gmail.com>
2024-11-25 16:16:04 +0800

    [Project #3 event_management_api] setting up email notification and mock with Mailpit

--- events_management/app/Console/Commands/SendEventReminders.php
+++ events_management/app/Console/Commands/SendEventReminders.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Event;
+use App\Notifications\EventReminderNotification;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 
@@ -38,7 +39,9 @@ class SendEventReminders extends Command
 
         $events->each(
             fn($event) => $event->attendees->each(
-                fn($attendee) => $this->info("Send reminder to " . $attendee->user->id . " for event " . $event->name)
+                fn($attendee) => $attendee->user->notify(
+                    new EventReminderNotification($event)
+                )
             )
         );
 
--- events_management/app/Console/Kernel.php
+++ events_management/app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:send-event-reminders')->everyMinute();
+        $schedule->command('app:send-event-reminders')->daily();
     }
 
     /**
