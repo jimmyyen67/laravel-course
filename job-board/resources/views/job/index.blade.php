134f9ace26de3a6e712fa1899c17b68d013ace99
JimmyYen <ichthyoid95@gmail.com>
2025-01-18 21:24:33 +0800

    [Project #5 job_board] experience radio search

--- job-board/resources/views/job/index.blade.php
+++ job-board/resources/views/job/index.blade.php
@@ -16,7 +16,25 @@
                         <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To"></x-text-input>
                     </div>
                 </div>
-                <div>3</div>
+                <div>
+                    <div class="mb-1 font-semibold">Experience</div>
+                    <label for="experience-all" class="mb-1 flex items-center">
+                      <input type="radio" id="experience-all" name="experience" value="" @checked(!request('experience'))>
+                      <span class="ml-2">All</span>
+                    </label>
+                    <label for="experience-entry" class="mb-1 flex items-center">
+                      <input type="radio" id="experience-entry" name="experience" value="entry" @checked('entry' === request('experience'))>
+                      <span class="ml-2">Entry</span>
+                    </label>
+                    <label for="experience-intermediate" class="mb-1 flex items-center">
+                      <input type="radio" id="experience-intermediate" name="experience" value="intermediate" @checked('intermediate' === request('experience'))>
+                      <span class="ml-2">Intermediate</span>
+                    </label>
+                    <label for="experience-senior" class="mb-1 flex items-center">
+                      <input type="radio" id="experience-senior" name="experience" value="senior" @checked('senior' === request('experience'))>
+                      <span class="ml-2">Senior</span>
+                    </label>
+                  </div>
                 <div>4</div>
             </div>
             <button class="w-full">Filter</button>
