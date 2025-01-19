476fc9fc0f46512dc8ffe33dbc98a709ef116005
JimmyYen <ichthyoid95@gmail.com>
2025-01-19 13:42:14 +0800

    [Project #5 job_board] empty input button and reset form

--- job-board/resources/views/components/text-input.blade.php
+++ job-board/resources/views/components/text-input.blade.php
@@ -1,2 +1,14 @@
-<input tpye="text" id={{ $name }} name="{{ $name }}" placeholder="{{ $placeholder }}" value="{{ $value }}"
-class="w-full rounded-md border-0 px-2.5 py-1.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2">
+<div class="relative">
+    @if ($formId)
+    <button type="button" class="absolute top-1/2 -translate-y-1/2 right-2 focus:outline-none"
+        onclick="document.getElementById('{{ $name }}').value = ''; document.getElementById('{{ $formId }}').submit();">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="size-6 text-slate-300 text-sm">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+        </svg>
+    </button>
+    @endif
+    <input tpye="text" id={{ $name }} name="{{ $name }}" placeholder="{{ $placeholder }}"
+        value="{{ $value }}"
+        class="pr-8 w-full rounded-md border-0 px-2.5 py-1.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2">
+</div>
