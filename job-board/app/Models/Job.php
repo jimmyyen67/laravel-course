de79ff4f13eee7571528ba72aebe5b1b0f0c6a41
JimmyYen <ichthyoid95@gmail.com>
2025-01-26 21:25:54 +0800

    [Project #5 job_board] job soft delete

--- job-board/app/Models/Job.php
+++ job-board/app/Models/Job.php
@@ -8,11 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Job extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
     protected $fillable = ['title', 'location', 'salary', 'description', 'experience', 'category'];
     public static array $experience = ['entry', 'intermediate', 'senior'];
     public static array $category = ['IT', 'Finance', 'Marketing', 'Sales'];
